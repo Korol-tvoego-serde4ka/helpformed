403d5f03f644adab450fbb27992f5d9b12ddc92b
Серива Роковой <64690281+Korol-tvoego-serde4ka@users.noreply.github.com>
2023-01-05 22:08:52 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,26 +3,24 @@ from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 
-class SyringeCounterApp(App):
+class SyringeCounter(App):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.syringes_used = 0
 
     def build(self):
-        # Create a button to increment the counter
-        button = Button(text='Use Syringe')
-        button.bind(on_press=self.increment_syringes)
+        # Create a button to increment the number of syringes used
+        button = Button(text='Use Syringe', on_press=self.increment_syringes)
 
-        # Create a label to display the counter
-        label = Label(text=f'Number of syringes used: {self.syringes_used}')
+        # Create a label to display the number of syringes used
+        self.label = Label(text=f'Number of syringes used: {self.syringes_used}')
 
-        # Return the button and label as the root widget
-        return button, label
+        return self.label
 
     def increment_syringes(self, instance):
-        # Increment the counter and update the label
+        # Increment the number of syringes used and update the label
         self.syringes_used += 1
-        self.root.children[1].text = f'Number of syringes used: {self.syringes_used}'
+        self.label.text = f'Number of syringes used: {self.syringes_used}'
 
 if __name__ == '__main__':
-    SyringeCounterApp().run()
+    SyringeCounter().run()
